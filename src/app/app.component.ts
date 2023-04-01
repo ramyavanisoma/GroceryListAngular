3df3a24656b302ea7177ee64f511c4078a726d21
ramyavanisoma <ramyavani.321@gmail.com>
2023-04-01 14:44:55 -0700

    Added Logic for strike off and added title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
   addToList(itemName:string){
       const item=itemName.charAt(0).toUpperCase()
       + itemName.slice(1);
-      this.items.push({name:item});
+      this.items.push({name:item, strike:false});
      
   }
 
@@ -31,5 +31,23 @@ export class AppComponent {
     }
    
   }
-}
+
+
+  onstrike(item:string){
   
+    for(var i = 0;i < this.items.length; i++){
+      if(item == this.items[i].name){
+        if(this.items[i].strike){
+          this.items[i].strike = false;
+        }
+        else{
+          this.items[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
+  
+  
+}
+
