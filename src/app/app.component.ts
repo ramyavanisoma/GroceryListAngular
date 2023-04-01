f06bc970b5afd45f7de9392d79de49dece255e30
ramyavanisoma <ramyavani.321@gmail.com>
2023-04-01 11:24:58 -0700

    added delete option and strike button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,15 @@ export class AppComponent {
   
   title = 'GroceryListAngular';
   items:any[];
-  
+
  constructor(){
   this.items=[];
  }
   
   addToList(itemName:string){
-
-      this.items.push({name:itemName});
+      const item=itemName.charAt(0).toUpperCase()
+      + itemName.slice(1);
+      this.items.push({name:item});
      
   }
 
